404e27f97ae0197de847a2674cf6226b129d4200
Alexander <88308276+alexanderluk1@users.noreply.github.com>
2025-12-04 09:19:26 +0800

    added deal function

--- 2-cards/main.go
+++ 2-cards/main.go
@@ -1,6 +1,14 @@
 package main
 
+import "fmt"
+
 func main() {
 	cards := newDeck()
-	cards.print()
+	// cards.print()
+
+	hand, remaining := cards.deal(5)
+
+	hand.print()
+	fmt.Println("---")
+	remaining.print()
 }
