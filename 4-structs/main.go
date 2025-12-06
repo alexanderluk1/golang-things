c89cdd2eed883af572567acbd03df4e479d56c5b
Alexander <88308276+alexanderluk1@users.noreply.github.com>
2025-12-06 14:52:35 +0800

    embedded struct in a struct

--- 4-structs/main.go
+++ 4-structs/main.go
@@ -1,10 +1,38 @@
 package main
 
+import "fmt"
+
+type contactInfo struct {
+	email string
+	zip int
+}
+
 type person struct {
 	firstName string
 	lastName string
+	contact contactInfo
 }
 
 func main() {
 	alex := person{firstName: "alex", lastName: "anderson"} // Creating a new struct
+	fmt.Println(alex)
+
+
+	var alex2 person
+	fmt.Printf("%#v", alex2)
+
+	alex2.firstName = "John"
+	alex2.lastName = "Hopper"
+	fmt.Printf("\n%#v", alex2)
+
+	jim := person {
+		firstName: "Jim",
+		lastName: "Hopper",
+		contact: contactInfo{
+			email: "jim@gmail.com", 
+			zip: 51952,
+		},
+	}
+
+	fmt.Printf("\n%+v", jim)
 }
